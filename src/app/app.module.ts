75df1c776775f742d1df5c4e8eefdf78f48f0a0f
Mark D <hooami@gmail.com>
2019-02-25 00:09:39 -0800

    install material, various schematics, gen user store, misc

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StoreModule } from '@ngrx/store';
+import { reducers, metaReducers } from './reducers';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
+import { EffectsModule } from '@ngrx/effects';
+import * as fromUser from './shared/store/user/user.reducer';
+import { UserEffects } from './shared/store/user/user.effects';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -10,7 +17,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StoreModule.forRoot(reducers, { metaReducers }),
+    !environment.production ? StoreDevtoolsModule.instrument() : [],
+    EffectsModule.forRoot([]),
+    StoreModule.forFeature('user', fromUser.reducer),
+    EffectsModule.forFeature([UserEffects]),
+    NoopAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
